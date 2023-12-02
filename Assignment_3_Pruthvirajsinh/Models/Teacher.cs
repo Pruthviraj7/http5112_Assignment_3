ef7c1808ed232540260527b054d54c3d3ae49943
Pruthvirajsinh Solanki <72735146+Pruthviraj7@users.noreply.github.com>
2023-12-01 22:15:56 -0500

    Update Teacher.cs

--- Assignment_3_Pruthvirajsinh/Models/Teacher.cs
+++ Assignment_3_Pruthvirajsinh/Models/Teacher.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -18,26 +19,33 @@ namespace Assignment_3_Pruthvirajsinh.Models
         /// <summary>
         /// Gets or sets the first name of the teacher.
         /// </summary>
+        [Required(ErrorMessage = "Please enter the first name.")]
         public string teacherfname;
 
         /// <summary>
         /// Gets or sets the last name of the teacher.
         /// </summary>
+        [Required(ErrorMessage = "Please enter the last name.")]
         public string teacherlname;
 
         /// <summary>
-        /// Gets or sets the unique employee number for the teacher.
+        /// Gets or sets the employee number of the teacher.
         /// </summary>
+        [Required(ErrorMessage = "Please enter the employee number.")]
         public string employeenumber;
 
         /// <summary>
         /// Gets or sets the salary of the teacher.
         /// </summary>
-        public decimal salary;
+        [Required(ErrorMessage = "Please enter the salary.")]
+        public double salary;
 
         /// <summary>
-        /// Gets or sets the ID of the associated class (nullable).
+        /// Gets or sets the class ID associated with the teacher.
         /// </summary>
         public int? ClassId;
+
+        [Required(ErrorMessage = "Please enter the hire date.")]
+        public DateTime HireDate { get; set; } = DateTime.Now;
     }
 }
